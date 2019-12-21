7c3679b6c1aa1c56aef8697bb2cc908e0b6ae58b
t4skforce <t4skforce1337@gmail.com>
2019-12-21 10:35:24 +0100

    Delete aria2-server.sh

--- aria2-server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -e
-aria2c --disable-ipv6=true --enable-rpc --rpc-allow-origin-all --continue=true --daemon=false --log=- --check-certificate=false --save-session=/var/local/aria2c/aria2c.sess --save-session-interval=2 --continue=true --input-file=/var/local/aria2c/aria2c.sess --rpc-save-upload-metadata=true --force-save=true --log-level=warn
